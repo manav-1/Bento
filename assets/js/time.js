992b8aad815ab79b1fb023c0340d80de8af943a0
Manav Arora <manav81101@gmail.com>
2023-12-15 15:54:06 +0530

    updates in configs

--- assets/js/time.js
+++ assets/js/time.js
@@ -5,27 +5,48 @@
 
 window.onload = displayClock();
 function displayClock() {
-	const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-	var d = new Date();
-	var mm = monthNames[d.getMonth()];
-	var dd = d.getDate();
-	var min = (mins = ('0' + d.getMinutes()).slice(-2));
-	var hh = d.getHours();
-	var ampm = '';
+  var d = new Date();
+  var mm = monthNames[d.getMonth()];
+  var dd = d.getDate();
+  var min = (mins = ("0" + d.getMinutes()).slice(-2));
+  var hh = d.getHours();
+  var ampm = "";
+  var ss = d.getSeconds();
 
-	if (CONFIG.twelveHourFormat) {
-		ampm = hh >= 12 ? ' pm' : ' am';
-		hh = hh % 12;
-		hh = hh ? hh : 12;
-	}
+  if (ss < 10) {
+    ss = "0" + ss;
+  }
 
-	document.getElementById('hour').innerText = hh;
-	document.getElementById('separator').innerHTML = ' : ';
-	document.getElementById('minutes').innerText = min + ampm;
+  if (CONFIG.twelveHourFormat) {
+    ampm = hh >= 12 ? " pm" : " am";
+    hh = hh % 12;
+    hh = hh ? hh : 12;
+  }
 
-	document.getElementById('month').innerText = mm;
-	document.getElementById('day').innerText = dd;
+  document.getElementById("hour").innerText = hh;
+  document.getElementById("separator1").innerHTML = " : ";
+  document.getElementById("minutes").innerText = min;
+  document.getElementById("separator2").innerHTML = " : ";
+  console.log(document.getElementById("seconds"));
+  document.getElementById("seconds").innerText = ss + ampm;
 
-	setTimeout(displayClock, 1000);
+  document.getElementById("month").innerText = mm;
+  document.getElementById("day").innerText = dd;
+
+  setTimeout(displayClock, 1000);
 }
